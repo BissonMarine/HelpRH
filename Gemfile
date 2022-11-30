256038c86fafcd00a057166a34e9389a6b0cc6ff
Marine Bisson <marinemaute@free.fr>
2022-11-30 10:46:24 +0100

    Cleaning

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
-gem 'oauth2'
-gem 'httparty'
 
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
   get "search", to: "pages#search"
   get "result", to: "pages#result"
 
-  #Routes-test pour l'API :
-  get '/oauth/callback', to: 'data#create_session'
-  get '/index', to: 'data#index'
-  # root to: 'data#index'
 end
